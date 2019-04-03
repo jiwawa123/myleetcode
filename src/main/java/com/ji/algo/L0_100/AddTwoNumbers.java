bbe2a5fd30eada5783bf447c7173afdcabbb404e
ji <252099562@qq.com>
2019-04-03 17:07:19 +0800

    myleetcode using java

---
+++ src/main/java/com/ji/algo/L0_100/AddTwoNumbers.java
@@ -0,0 +1,24 @@
+package com.ji.algo.L0_100;/*
+    user ji
+    data 2019/4/3
+    time 4:59 PM
+*/
+
+import com.ji.algo.Util.ListNode;
+
+import java.util.List;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //判断两个链表都是空的情况
+        if(l1==null&&l2==null)
+            return null;
+
+        ListNode target = new ListNode(0);
+
+        return target;
+    }
+}
---
+++ src/main/java/com/ji/algo/Util/ListNode.java
@@ -0,0 +1,17 @@
+package com.ji.algo.Util;/*
+    user ji
+    data 2019/4/3
+    time 4:58 PM
+*/
+/*
+工具包->链表的工具包
+ */
+public class ListNode {
+    public int value;
+    public ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+}
