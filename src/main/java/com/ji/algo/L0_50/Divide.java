9fde80ff20ee3cbc6dffbd8cc8cc2c3aa9620160
ji <252099562@qq.com>
2019-04-08 22:10:50 +0800

    ques29,反向求解使用<< 操作，>> 可以实现乘法

---
+++ src/main/java/com/ji/algo/L0_50/Divide.java
@@ -0,0 +1,34 @@
+package com.ji.algo.L0_50;/*
+    user ji
+    data 2019/4/8
+    time 9:59 PM
+*/
+
+public class Divide {
+    public static void main(String[] args) {
+        System.out.println(3<<3);
+    }
+
+    public int divide(int dividend, int divisor) {
+        if (dividend == 0) {
+            return 0;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean negative;
+        negative = (dividend ^ divisor) <0;//用异或来计算是否符号相异
+        long t = Math.abs((long) dividend);
+        long d= Math.abs((long) divisor);
+        int result = 0;
+        //反向求解
+        for (int i=31; i>=0;i--) {
+            if ((t>>i)>=d) {//找出足够大的数2^n*divisor
+                result+=1<<i;//将结果加上2^n
+                t-=d<<i;//将被除数减去2^n*divisor
+            }
+        }
+        return negative ? -result : result;//符号相异取反
+
+    }
+}
