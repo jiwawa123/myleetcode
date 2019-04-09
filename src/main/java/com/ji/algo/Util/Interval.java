374f0f64cc376222a958297690bf316c3ccd6929
ji <252099562@qq.com>
2019-04-09 11:17:47 +0800

    添加工具类 interval

---
+++ src/main/java/com/ji/algo/Util/Interval.java
@@ -0,0 +1,20 @@
+package com.ji.algo.Util;/*
+    user ji
+    data 2019/4/9
+    time 11:16 AM
+*/
+
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
