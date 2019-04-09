863c53a3c3572a5fe58899425acf788e6f57e79c
ji <252099562@qq.com>
2019-04-09 11:36:27 +0800

    ques56，使用collections直接对其进行排序，然后指定一系列的规则即可

---
+++ src/main/java/com/ji/algo/L51_100/MergeInterval.java
@@ -0,0 +1,59 @@
+package com.ji.algo.L51_100;/*
+    user ji
+    data 2019/4/9
+    time 11:18 AM
+*/
+
+import com.ji.algo.Util.Interval;
+
+import java.util.*;
+
+public class MergeInterval {
+    public static void main(String[] args) {
+        List<Interval> l = new ArrayList<>();
+        l.add(new Interval(1,4));
+        l.add(new Interval(0,4));
+        merge(l);
+    }
+
+    public static List<Interval> merge(List<Interval> intervals) {
+        List<Interval> answer = new ArrayList<>();
+        if (null == intervals || intervals.size() == 0)
+            return answer;
+        //使用流对其进行排序
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                if(o1.start!=o2.start)
+                    return o1.start - o2.start;
+                return o1.end - o2.end;
+            }
+        });
+
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval tmp = intervals.get(i);
+            if (start == tmp.start && end == tmp.end)
+                continue;
+            if (start == tmp.start && end <= tmp.end) {
+                end = tmp.end;
+                continue;
+            }
+
+            if (end < tmp.start) {
+                answer.add(new Interval(start, end));
+                start = tmp.start;
+                end = tmp.end;
+                continue;
+            }
+            if(start<tmp.start&&end<tmp.end){
+                end = tmp.end;
+            }
+
+        }
+        answer.add(new Interval(start, end));
+        return answer;
+
+    }
+}
