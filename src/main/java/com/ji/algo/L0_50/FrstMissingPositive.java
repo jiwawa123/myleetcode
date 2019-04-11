ed6a5a6781bcd6792fca15966499a74cc5f2edda
ji <252099562@qq.com>
2019-04-11 08:47:57 +0800

    ques41,修改代码，提高时间复杂度

--- src/main/java/com/ji/algo/L0_50/FrstMissingPositive.java
+++ src/main/java/com/ji/algo/L0_50/FrstMissingPositive.java
@@ -11,14 +11,16 @@ public class FrstMissingPositive {
     public static void main(String[] args) {
 
     }
+
     public int firstMissingPositive(int[] nums) {
         int res = 1;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], 1);
-            while (map.containsKey(res)) {
-                res++;
-            }
+
+        }
+        while (map.containsKey(res)) {
+            res++;
         }
         return res;
     }
