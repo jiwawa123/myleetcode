f94cd9e340a79cd33ab0f9f1ca17dc37448722d2
ji <252099562@qq.com>
2019-04-11 22:28:43 +0800

    ques107,可以使用collections.reverse或者在list的index = 0的位置插入都可以

---
+++ src/main/java/com/ji/algo/L101_105/LevelOrderBottom.java
@@ -0,0 +1,40 @@
+package com.ji.algo.L101_105;/*
+    user ji
+    data 2019/4/11
+    time 10:24 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.*;
+
+public class LevelOrderBottom {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root==null)
+            return list;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> tmp = new ArrayList<>();
+            Queue<TreeNode> que = new LinkedList<>();
+            while(!queue.isEmpty()){
+                TreeNode tt = queue.remove();
+                tmp.add(tt.value);
+                if(tt.left!=null){
+                    que.add(tt.left);
+                }
+                if(tt.right!=null){
+                    que.add(tt.right);
+                }
+            }
+            queue = que;
+            list.add(tmp);
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
