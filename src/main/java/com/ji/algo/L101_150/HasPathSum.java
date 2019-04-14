c01eef417e7c58124b190b74c777ea0daa299681
ji <252099562@qq.com>
2019-04-14 12:02:38 +0800

    ques112,判断一个节点是不是一个叶子节点的时候直接在这一层判断

---
+++ src/main/java/com/ji/algo/L101_150/HasPathSum.java
@@ -0,0 +1,22 @@
+package com.ji.algo.L101_150;/*
+    user ji
+    data 2019/4/14
+    time 11:50 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class HasPathSum {
+    public static void main(String[] args) {
+
+    }
+    //直接在当前节点的位置直接终结即可
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        if (root.value == sum && (root.left == null && root.right == null))
+            return true;
+        return hasPathSum(root.left, sum - root.value)
+                || hasPathSum(root.right, sum - root.value);
+    }
+}
