41966180d447e1d2eaa8db3834f67e16648226e1
ji <252099562@qq.com>
2019-04-14 12:03:04 +0800

    ques113，list在传递的时候使用新值

---
+++ src/main/java/com/ji/algo/L101_150/PathSum.java
@@ -0,0 +1,36 @@
+package com.ji.algo.L101_150;/*
+    user ji
+    data 2019/4/14
+    time 11:55 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSum {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+        help(list,tmp,root,sum);
+        return list;
+    }
+    public void help(List<List<Integer>> re,List<Integer> tmp ,TreeNode root,int sum){
+        if(root==null)
+            return;
+        if(root.value==sum&&root.left==null&&root.right==null)
+        {
+            tmp.add(root.value);
+            re.add(new ArrayList<>(tmp));
+        }else{
+            tmp.add(root.value);
+            help(re,new ArrayList<>(tmp),root.left,sum - root.value);
+            help(re,new ArrayList<>(tmp),root.right,sum - root.value);
+        }
+    }
+}
