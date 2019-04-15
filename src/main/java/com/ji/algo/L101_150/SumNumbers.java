fca4193b91b92ea8e860a627f26ba9b6ce34bf2a
ji <252099562@qq.com>
2019-04-15 15:29:14 +0800

    ques129，通过全局变量以及help递归函数即可

---
+++ src/main/java/com/ji/algo/L101_150/SumNumbers.java
@@ -0,0 +1,34 @@
+package com.ji.algo.L101_150;/*
+    user ji
+    data 2019/4/15
+    time 3:25 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    int count = 0;
+
+    public int sumNumbers(TreeNode root) {
+        if (null == root)
+            return 0;
+        help_sum(root, "");
+        return count;
+    }
+
+    public void help_sum(TreeNode root, String s) {
+        if(root==null)
+            return;
+        if (root.left == null && root.right == null) {
+            s += root.value;
+            count+=Integer.valueOf(s);
+            return;
+        }
+        help_sum(root.left,s+root.value);
+        help_sum(root.right,s+root.value);
+    }
+}
