7f45f5b76df7a3d9e602815f4042df0642c23c30
ji <252099562@qq.com>
2019-04-17 09:04:19 +0800

    ques105,依旧使用递归的方法，先确定左边，然后确定先序遍历对应的左边，同理找到右边。Arrays.copyOfRange的使用

---
+++ src/main/java/com/ji/algo/L101_150/BuildTree.java
@@ -0,0 +1,37 @@
+package com.ji.algo.L101_150;/*
+    user ji
+    data 2019/4/17
+    time 8:51 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.Arrays;
+
+public class BuildTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (null == preorder || preorder.length == 0)
+            return null;
+        if (preorder.length == 1)
+            return new TreeNode(preorder[0]);
+        TreeNode root = new TreeNode(preorder[0]);
+        int index = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == preorder[0]) {
+                index = i;
+                break;
+            }
+        }
+        int left[] = Arrays.copyOfRange(inorder, 0, index - 1);
+        int right[] = Arrays.copyOfRange(inorder, index + 1, inorder.length);
+        int preLeft[] = Arrays.copyOfRange(preorder, 1, 1 + left.length);
+        int preRight[] = Arrays.copyOfRange(preorder, 1 + preLeft.length, preorder.length);
+        root.left = buildTree(preLeft, left);
+        root.right = buildTree(preRight, right);
+        return root;
+    }
+}
