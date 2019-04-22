db19bcca6cbac658facdc89921910a8f3dd7780b
ji <252099562@qq.com>
2019-04-22 19:53:49 +0800

    ques224,测试的类很奇葩

--- src/main/java/com/ji/algo/L201_250/CalculateII.java
+++ src/main/java/com/ji/algo/L201_250/CalculateII.java
@@ -1,19 +1,47 @@
 package com.ji.algo.L201_250;/*
     user ji
     data 2019/4/22
-    time 7:44 AM
+    time 7:19 PM
 */
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class Calculate {
+public class CalculateII {
     public static void main(String[] args) {
-        System.out.println(calculate("1-1+1"));
+        System.out.println(calculate("2-(5-6)"));
     }
 
     public static int calculate(String s) {
+        if (!s.contains("(")) {
+            return calculateII(s);
+        } else {
+            Stack<Integer> stack = new Stack<>();
+            int i = 0;
+            while (i < s.length()) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                    i++;
+                    continue;
+                }
+                if (s.charAt(i) == ')') {
+                    int index = stack.pop();
+                    int k = calculateII(s.substring(index + 1, i));
+                    
+                    s = s.substring(0, index) + k + s.substring(i + 1, s.length());
+
+                    i = index;
+                    continue;
+                }
+                i++;
+            }
+        }
+
+        return calculateII(s);
+    }
+
+    public static int calculateII(String s) {
         Stack<Long> stack = new Stack<>();
         Stack<Character> stack1 = new Stack<>();
         s = s.replaceAll(" ", "");
@@ -26,17 +54,17 @@ public class Calculate {
                 continue;
             }
             if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-                if(!tmp.equals("")){
+                if (!tmp.equals("")) {
                     stack.push(Long.valueOf(tmp));
                     tmp = "";
                 }
-                stack1.push(s.charAt(i)=='+'?'+':'-');
+                stack1.push(s.charAt(i) == '+' ? '+' : '-');
                 i++;
 
                 continue;
             }
             if (s.charAt(i) == '*' || s.charAt(i) == '/') {
-                if(!tmp.equals("")){
+                if (!tmp.equals("")) {
                     stack.push(Long.valueOf(tmp));
                     tmp = "";
                 }
@@ -60,25 +88,25 @@ public class Calculate {
             stack.push(Long.valueOf(tmp));
         }
         StringBuffer sp = new StringBuffer();
-        while(!stack1.isEmpty()){
+        while (!stack1.isEmpty()) {
             sp.append(stack1.pop());
         }
         sp = sp.reverse();
         List<Long> ll = new ArrayList();
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             ll.add(stack.pop());
         }
-        long re = ll.get(ll.size()-1);
-        int index = ll.size() -2;
+        long re = ll.get(ll.size() - 1);
+        int index = ll.size() - 2;
         int j = 0;
-        while(j<sp.length()){
-            if(sp.charAt(j)=='+'){
-                re+=ll.get(index--);
-            }else{
-                re-=ll.get(index--);
+        while (j < sp.length()) {
+            if (sp.charAt(j) == '+') {
+                re += ll.get(index--);
+            } else {
+                re -= ll.get(index--);
             }
             j++;
         }
-        return (int)re;
+        return (int) re;
     }
 }
