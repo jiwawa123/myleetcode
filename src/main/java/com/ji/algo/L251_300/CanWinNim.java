6b6a2f5d849d36135382636ab29926cbf6359304
ji <252099562@qq.com>
2019-04-23 21:21:17 +0800

    ques292,数学游戏抢30

---
+++ src/main/java/com/ji/algo/L251_300/CanWinNim.java
@@ -0,0 +1,15 @@
+package com.ji.algo.L251_300;/*
+    user ji
+    data 2019/4/23
+    time 9:20 PM
+*/
+
+public class CanWinNim {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
