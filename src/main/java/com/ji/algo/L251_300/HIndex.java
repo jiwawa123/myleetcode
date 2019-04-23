2a5815fc4ab6c189de5a9f7a91bdc1e0d8545aa3
ji <252099562@qq.com>
2019-04-23 20:49:45 +0800

    ques274,Hindex指数，直接使用排序比较即可

---
+++ src/main/java/com/ji/algo/L251_300/HIndex.java
@@ -0,0 +1,24 @@
+package com.ji.algo.L251_300;/*
+    user ji
+    data 2019/4/23
+    time 8:47 PM
+*/
+
+import java.util.Arrays;
+
+public class HIndex {
+    public static void main(String[] args) {
+
+    }
+
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int len = citations.length;
+        for (int i = 0; i < len; i++) {
+            if (citations[i] >= len - i) {
+                return len - i;
+            }
+        }
+        return 0;
+    }
+}
