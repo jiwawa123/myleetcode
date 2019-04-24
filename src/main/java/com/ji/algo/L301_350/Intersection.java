9db871d69c1c9da9505cfb4f8e06387381657776
ji <252099562@qq.com>
2019-04-24 21:25:58 +0800

    ques350,使用map来寻找两个数组之间的交集

--- src/main/java/com/ji/algo/L301_350/Intersection.java
+++ src/main/java/com/ji/algo/L301_350/Intersection.java
@@ -4,10 +4,7 @@ package com.ji.algo.L301_350;/*
     time 8:15 PM
 */
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Intersection {
     public static void main(String[] args) {
@@ -28,6 +25,29 @@ public class Intersection {
             }
         }
         List<Integer> tmp = new ArrayList<>(list);
+        int arr[] = new int[tmp.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = tmp.get(i);
+        }
+        return arr;
+    }
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> tmp = new ArrayList<>();
+        Map<Integer,Integer> map = new HashMap();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i],map.getOrDefault(nums2[i],0));
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            if(map.containsKey(nums1[i])){
+                if(map.get(nums1[i])==1){
+                    tmp.add(nums1[i]);
+                    map.remove(nums1[i]);
+                }else{
+                    tmp.add(nums1[i]);
+                    map.put(nums1[i],map.get(nums1[i])-1);
+                }
+            }
+        }
         int arr[] = new int[tmp.size()];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = tmp.get(i);
