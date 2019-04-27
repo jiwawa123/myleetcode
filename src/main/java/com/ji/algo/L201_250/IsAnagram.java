2fd7ea24f5bc6f9a2cfdaa2b52ba0d09953d5b16
ji <252099562@qq.com>
2019-04-27 13:33:34 +0800

    ques242,使用数组实现相应的有效的字母异位问题

--- src/main/java/com/ji/algo/L201_250/IsAnagram.java
+++ src/main/java/com/ji/algo/L201_250/IsAnagram.java
@@ -28,4 +28,25 @@ public class IsAnagram {
         return String.valueOf(s1).equals(String.valueOf(t1));
 
     }
+
+    public boolean isAnagramII(String s, String t) {
+        if (s == null && t == null)
+            return true;
+        if (s == null || t == null)
+            return false;
+        if (s.length() != t.length())
+            return false;
+        int arr[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a'] += 1;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (arr[t.charAt(i) - 'a'] <= 0) {
+                return false;
+            } else {
+                arr[t.charAt(i) - 'a'] -= 1;
+            }
+        }
+        return true;
+    }
 }
