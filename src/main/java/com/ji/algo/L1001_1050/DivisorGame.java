6383417fe3649e045ce2b0767065e9b87810a52d
ji <252099562@qq.com>
2019-04-29 20:49:48 +0800

    ques1025，除法博弈

---
+++ src/main/java/com/ji/algo/L1001_1050/DivisorGame.java
@@ -0,0 +1,11 @@
+package com.ji.algo.L1001_1050;/*
+    user ji
+    data 2019/4/29
+    time 7:58 PM
+*/
+
+public class DivisorGame {
+    public boolean divisorGame(int N) {
+        return N%2==0;
+    }
+}
