36c66f178463ab13593c36c33c4a212047fa86d6
ji <252099562@qq.com>
2019-05-02 13:31:03 +0800

    ques338，比特位计数,数学问题寻找规律即可

--- src/main/java/com/ji/algo/L301_350/CountBits.java
+++ src/main/java/com/ji/algo/L301_350/CountBits.java
@@ -8,29 +8,22 @@ import java.util.Arrays;
 
 public class CountBits {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(countBits(8)));
+        System.out.println(Arrays.toString(countBits(5)));
     }
 
     public static int[] countBits(int num) {
         int res[] = new int[num + 1];
         res[0] = 0;
-        int tmp = 0;
+        int tmp = 1;
         for (int i = 1; i <= num; i++) {
-            if (isPan(i)) {
+            if (i == tmp*2) {
                 res[i] = 1;
-                tmp = i;
+                tmp *= 2;
                 continue;
             } else {
                 res[i] = 1 + res[i - tmp];
             }
         }
         return res;
-    }
-
-    public static boolean isPan(int num) {
-        while (num % 2 == 0) {
-            num /= 2;
-        }
-        return num == 1;
     }
 }
