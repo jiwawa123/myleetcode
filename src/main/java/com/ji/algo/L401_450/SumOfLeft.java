44ab085c0eda9fda4f806ba15c0c42a6cc5c35db
ji <252099562@qq.com>
2019-05-03 09:18:33 +0800

    ques404,左叶子之和，使用标识符判断是不是左叶子

---
+++ src/main/java/com/ji/algo/L401_450/SumOfLeft.java
@@ -0,0 +1,34 @@
+package com.ji.algo.L401_450;/*
+    user ji
+    data 2019/5/3
+    time 9:14 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class SumOfLeft {
+    public static void main(String[] args) {
+
+    }
+
+    int count = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null)
+            return 0;
+        help(root.left, true);
+        help(root.right, false);
+        return count;
+    }
+
+    public void help(TreeNode root, boolean flag) {
+        if (root == null)
+            return;
+        if (flag && root != null && root.left == null && root.right == null) {
+            count += root.value;
+            return;
+        }
+        help(root.left, true);
+        help(root.right, false);
+    }
+}
