92127409b69f92c8546c6342bea85e57bce8d5b8
ji <252099562@qq.com>
2019-05-06 21:18:55 +0800

    ques705,自己设计的hashset，在数字范围有限的情况下直接使用数组完成

---
+++ src/main/java/com/ji/algo/L701_750/MyHashSet.java
@@ -0,0 +1,28 @@
+package com.ji.algo.L701_750;/*
+    user ji
+    data 2019/5/6
+    time 9:12 PM
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyHashSet {
+    boolean[] map = new boolean[1000005];
+    public MyHashSet() {
+
+    }
+
+    public void add(int key) {
+        map[key] = true;
+    }
+
+    public void remove(int key) {
+        map[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return map[key] == true;
+    }
+}
