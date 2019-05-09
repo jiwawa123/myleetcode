98448a6e9bf6b46eb355a7abc1f91314df2d344e
ji <252099562@qq.com>
2019-05-09 17:04:55 +0800

    ques513，寻找二叉树最左下边的数值

---
+++ src/main/java/com/ji/algo/L501_550/FindBottomLeftValue.java
@@ -0,0 +1,39 @@
+package com.ji.algo.L501_550;/*
+    user ji
+    data 2019/5/9
+    time 4:59 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class FindBottomLeftValue {
+    public static void main(String[] args) {
+
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            list = new ArrayList<>();
+            Queue<TreeNode> que = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                TreeNode tmp = queue.remove();
+                list.add(tmp.value);
+                if (tmp.left != null)
+                    que.add(tmp.left);
+                if (tmp.right != null)
+                    que.add(tmp.right);
+            }
+            queue = que;
+        }
+        return list.get(0);
+
+    }
+}
