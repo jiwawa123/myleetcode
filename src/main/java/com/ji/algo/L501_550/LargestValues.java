ee9cfae2694010aaaaf24fe8d9c1404984d39c59
ji <252099562@qq.com>
2019-05-09 17:08:34 +0800

    ques515，寻找树中每个行最大的元素

---
+++ src/main/java/com/ji/algo/L501_550/LargestValues.java
@@ -0,0 +1,39 @@
+package com.ji.algo.L501_550;/*
+    user ji
+    data 2019/5/9
+    time 5:05 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LargestValues {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> que = new LinkedList<>();
+            int val = Integer.MIN_VALUE;
+            while (!queue.isEmpty()) {
+                TreeNode tmp = queue.remove();
+                val = Math.max(val,tmp.value);
+                if (tmp.left != null)
+                    que.add(tmp.left);
+                if (tmp.right != null)
+                    que.add(tmp.right);
+            }
+            queue = que;
+            list.add(val);
+        }
+        return list;
+    }
+}
