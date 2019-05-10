2573aa642889552e0642c57de75d295acc2453f1
ji <252099562@qq.com>
2019-05-10 14:13:44 +0800

    ques637, 二叉树的层平均值，防止数字溢出

---
+++ src/main/java/com/ji/algo/L601_650/AverageLevel.java
@@ -0,0 +1,42 @@
+package com.ji.algo.L601_650;/*
+    user ji
+    data 2019/5/10
+    time 2:05 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageLevel {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new LinkedList<>();
+        if (root == null)
+            return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> que = new LinkedList<>();
+            int len = 0;
+            int sum = 0;
+            while (!queue.isEmpty()) {
+                TreeNode tmp = queue.remove();
+                sum += tmp.value;
+                len++;
+                if (tmp.left != null)
+                    que.add(tmp.left);
+                if (tmp.right != null)
+                    que.add(tmp.right);
+            }
+            result.add(sum * 1.0 / len);
+            queue = que;
+        }
+        return result;
+    }
+}
