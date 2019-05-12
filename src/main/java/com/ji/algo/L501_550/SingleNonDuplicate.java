39a63a4be988ae56b7171a2c3ba3c33965d2b97a
ji <252099562@qq.com>
2019-05-12 10:04:45 +0800

    ques540,二分查找，确保区间都是一个奇数长度

---
+++ src/main/java/com/ji/algo/L501_550/SingleNonDuplicate.java
@@ -0,0 +1,27 @@
+package com.ji.algo.L501_550;/*
+    user ji
+    data 2019/5/12
+    time 9:59 AM
+*/
+
+public class SingleNonDuplicate {
+    public static void main(String[] args) {
+
+    }
+
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (m % 2 == 1) {
+                m--;   // 保证 l/h/m 都在偶数位，使得查找区间大小一直都是奇数
+            }
+            if (nums[m] == nums[m + 1]) {
+                l = m + 2;
+            } else {
+                h = m;
+            }
+        }
+        return nums[l];
+    }
+}
