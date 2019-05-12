a91345b9c69a3088d0b7bb520b708b6c7df8a0a5
ji <252099562@qq.com>
2019-05-12 09:43:55 +0800

    ques538,使用右->根->左的顺序遍历二叉树

---
+++ src/main/java/com/ji/algo/L501_550/ConvertBST.java
@@ -0,0 +1,28 @@
+package com.ji.algo.L501_550;/*
+    user ji
+    data 2019/5/12
+    time 9:38 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class ConvertBST {
+    public static void main(String[] args) {
+
+    }
+
+    public int preNum = 0;
+    //递归写法
+    public TreeNode convertBST(TreeNode root) {
+        unPreOrder(root);
+        return root;
+    }
+    public void unPreOrder(TreeNode root){
+        if(root == null)
+            return;
+        unPreOrder(root.right);
+        root.value += preNum;
+        preNum = root.value;
+        unPreOrder(root.left);
+    }
+}
