e4fb1dfbbd3d8c80861fb7fa5d900a8a63022eb7
ji <252099562@qq.com>
2019-05-14 17:23:54 +0800

    ques786,使用中序遍历即可

---
+++ src/main/java/com/ji/algo/L751_800/MinDiff.java
@@ -0,0 +1,31 @@
+package com.ji.algo.L751_800;/*
+    user ji
+    data 2019/5/14
+    time 5:21 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class MinDiff {
+    public static void main(String[] args) {
+
+    }
+    TreeNode pre;
+    int min = Integer.MAX_VALUE;
+    public int minDiffInBST(TreeNode root) {
+        help(root);
+        return  min;
+    }
+    public void help(TreeNode treeNode){
+        if(treeNode == null){
+            return;
+        }else {
+            help(treeNode.left);
+            if(pre != null  && Math.abs(treeNode.value - pre.value) < min){
+                min = Math.abs(treeNode.value - pre.value);
+            }
+            pre = treeNode;
+            help(treeNode.right);
+        }
+    }
+}
