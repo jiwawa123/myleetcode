dc217c0041d6b9775bb1a268dd772abebdd4fb18
ji <252099562@qq.com>
2019-05-23 23:06:36 +0800

    ques965,使用非递归算法实现对于二叉树的遍历，判断是不是单值二叉树

---
+++ src/main/java/com/ji/algo/L951_1000/UniqueValue.java
@@ -0,0 +1,35 @@
+package com.ji.algo.L951_1000;/*
+    user ji
+    data 2019/5/23
+    time 11:01 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.Stack;
+
+public class UniqueValue {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null)
+            return true;
+        int k = root.value;
+        Stack<TreeNode> stack = new Stack();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                if (root.value != k)
+                    return false;
+                else
+                    stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                root = root.right;
+            }
+        }
+        return true;
+    }
+}
