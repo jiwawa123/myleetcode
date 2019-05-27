2fd84335144a3633d913b9de95771864f0dba04b
ji <252099562@qq.com>
2019-05-27 22:50:12 +0800

    ques530. 二叉搜索树的最小绝对差使用非递归遍历即可

---
+++ src/main/java/com/ji/algo/L501_550/GetMinimumDifference.java
@@ -0,0 +1,37 @@
+package com.ji.algo.L501_550;/*
+    user ji
+    data 2019/5/27
+    time 10:43 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.Stack;
+
+public class GetMinimumDifference {
+    public static void main(String[] args) {
+
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        boolean flag = false;
+        int tmp = 0;
+        int count = 0;
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                if (flag) {
+                    count = Math.min(count, Math.abs(tmp - root.value));
+                } else {
+                    tmp = root.value;
+                }
+                root = root.right;
+            }
+        }
+        return count;
+    }
+}
