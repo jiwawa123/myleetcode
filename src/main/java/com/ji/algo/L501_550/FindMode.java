33e0ad0e0e0d5cc13255199eeaff9f4485ffdada
ji <252099562@qq.com>
2019-05-27 21:30:01 +0800

    ques501. 二叉搜索树中的众数,使用二叉树的非递归调用即可

---
+++ src/main/java/com/ji/algo/L501_550/FindMode.java
@@ -0,0 +1,47 @@
+package com.ji.algo.L501_550;/*
+    user ji
+    data 2019/5/27
+    time 9:22 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.*;
+
+public class FindMode {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findMode(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<TreeNode> statck = new Stack<>();
+        List<Integer> tmp = new LinkedList<>();
+        int count = 0;
+        while (!statck.isEmpty() || root != null) {
+            if (root != null) {
+                map.put(root.value, map.getOrDefault(root.value, 0) + 1);
+                statck.push(root);
+                root = root.left;
+            } else {
+                root = statck.pop();
+                root = root.right;
+            }
+        }
+        for (int k : map.keySet()
+                ) {
+            if (map.get(k) > count) {
+                tmp = new LinkedList<>();
+                tmp.add(k);
+                count = map.get(k);
+            } else if (map.get(k) == count) {
+                tmp.add(k);
+            }
+        }
+        int res[] = new int[tmp.size()];
+        for (int i = 0; i < tmp.size(); i++) {
+            res[i] = tmp.get(i);
+        }
+        return res;
+    }
+}
