d833f0a504e0fcd387f86b94101da78287655887
ji <252099562@qq.com>
2019-06-02 12:49:24 +0800

    ques976. 三角形的最大周长,对代码进行相应的优化

--- src/main/java/com/ji/algo/L951_1000/LargestPerimeter.java
+++ src/main/java/com/ji/algo/L951_1000/LargestPerimeter.java
@@ -16,20 +16,12 @@ public class LargestPerimeter {
         int count = 0;
         Arrays.sort(A);
         for (int i = A.length - 1; i >= 2; i--) {
-            if(count>3*A[i])
+            if(A[i]*3<count)
                 break;
-            for (int j = i - 1; j >= 1; j--) {
-                if(count>A[i]+2*A[j])
-                    break;
-                for (int k = j - 1; k >= 0; k--) {
-                    if (count > A[i] + A[j] + A[k])
-                        break;
-                    if (A[i] < A[j] + A[k] && A[k] < A[i] + A[j] && A[j] < A[i] + A[k]) {
-                        count = Math.max(count, A[i] + A[k] + A[j]);
-                    } else {
-                        break;
-                    }
-                }
+            int j = i - 1;
+            int k = i - 2;
+            if (A[i] < A[j] + A[k] && A[k] < A[i] + A[j] && A[j] < A[i] + A[k]) {
+                count = Math.max(count, A[i] + A[k] + A[j]);
             }
         }
         return count;
