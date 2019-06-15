953f00bff1d0912bdb058c13cee233848a6349da
ji <252099562@qq.com>
2019-06-15 09:45:22 +0800

    ques814. 二叉树剪枝，直接使用递归遍历是否需要删除即可

---
+++ src/main/java/com/ji/algo/L851_900/PruneTree.java
@@ -0,0 +1,27 @@
+package com.ji.algo.L851_900;/*
+    user ji
+    data 2019/6/15
+    time 9:44 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class PruneTree {
+    public static void main(String[] args) {
+
+    }
+    public TreeNode pruneTree(TreeNode root) {
+        if(!one(root))
+            return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return root;
+    }
+    public boolean one(TreeNode root){
+        if(root==null)
+            return false;
+        if(root.value==1)
+            return true;
+        return one(root.left)||one(root.right);
+    }
+}
