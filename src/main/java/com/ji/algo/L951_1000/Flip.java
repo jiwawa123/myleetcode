017ea16d1f554be6846166dc85a888729245ad6f
ji <252099562@qq.com>
2019-06-16 17:00:44 +0800

    ques951,反转等价二叉树

---
+++ src/main/java/com/ji/algo/L951_1000/Flip.java
@@ -0,0 +1,18 @@
+package com.ji.algo.L951_1000;/*
+    user ji
+    data 2019/6/16
+    time 5:00 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class Flip {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1==null&&root2==null)
+            return true;
+        if((root1==null||root2==null)||root1.value!=root2.value)
+            return false;
+        return (flipEquiv(root1.left,root2.right)&&flipEquiv(root1.right,root2.left))||
+                (flipEquiv(root1.left,root2.left)&&flipEquiv(root1.right,root2.right));
+    }
+}
