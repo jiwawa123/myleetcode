7751ad61b2eb01edcea84e64089848cbd9f3f1e6
ji <252099562@qq.com>
2019-06-19 13:54:58 +0800

    ques701. 二叉搜索树中的插入操作,使用递归即可

---
+++ src/main/java/com/ji/algo/L701_750/InsertIntoBST.java
@@ -0,0 +1,23 @@
+package com.ji.algo.L701_750;/*
+    user ji
+    data 2019/6/19
+    time 1:54 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class InsertIntoBST {
+    public static void main(String[] args) {
+
+    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null)
+            return new TreeNode(val);
+        if(root.value>val){
+            root.left = insertIntoBST( root.left,  val);
+        }else{
+            root.right = insertIntoBST(root.right,  val);
+        }
+        return root;
+    }
+}
