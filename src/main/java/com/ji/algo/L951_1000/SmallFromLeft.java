245adcd64da489db929faaee849d33169ca579b7
ji <252099562@qq.com>
2019-06-28 21:24:57 +0800

    ques988. 从叶结点开始的最小字符串,注意Java原生的字符串比较的方法

---
+++ src/main/java/com/ji/algo/L951_1000/SmallFromLeft.java
@@ -0,0 +1,48 @@
+package com.ji.algo.L951_1000;/*
+    user ji
+    data 2019/6/28
+    time 9:12 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SmallFromLeft {
+    public static void main(String[] args) {
+        int a = 25;
+        System.out.println((char) (a + 'a'));
+    }
+
+    List<String> res = new ArrayList<>();
+
+    public String smallestFromLeaf(TreeNode root) {
+        if (root == null)
+            return "";
+        helper(root, "");
+        Collections.sort(res, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return res.get(0);
+    }
+
+    public void helper(TreeNode root, String str) {
+        if (root == null)
+            return;
+        StringBuilder sp = new StringBuilder();
+        sp.append((char) (root.value + 'a'));
+        sp.append(str);
+        if (root.left == null || root.right == null) {
+            res.add(sp.toString());
+            return;
+        }
+        helper(root.left, sp.toString());
+        helper(root.right, sp.toString());
+    }
+}
