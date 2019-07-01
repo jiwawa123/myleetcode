c0b86b3cc4da6466648f7272aa26febe608c9541
ji <252099562@qq.com>
2019-07-01 10:20:01 +0800

    ques998,最大二叉树II。

---
+++ src/main/java/com/ji/algo/L951_1000/InsertIntoMaxTree.java
@@ -0,0 +1,22 @@
+package com.ji.algo.L951_1000;/*
+    user ji
+    data 2019/7/1
+    time 10:18 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class InsertIntoMaxTree {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+        if (root.value < val) {
+            TreeNode tmp = new TreeNode(val);
+            tmp.left = root;
+            return tmp;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+
+    }
+}
