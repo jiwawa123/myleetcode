ad225fd007a5e5a1d36c8c6df712804cbab8f625
ji <252099562@qq.com>
2019-07-08 13:04:19 +0800

    ques1109. 航班预订统计

---
+++ src/main/java/com/ji/algo/L1101_1150/CorpFlightBookings.java
@@ -0,0 +1,27 @@
+package com.ji.algo.L1101_1150;/*
+    user ji
+    data 2019/7/8
+    time 11:27 AM
+*/
+
+import java.util.Arrays;
+
+public class CorpFlightBookings {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int arr[] = new int[n+1];
+        for (int tmp[] : bookings
+                ) {
+            arr[tmp[0] - 1] += tmp[2];
+            arr[tmp[1]] -= tmp[2];
+        }
+        for (int i = 1; i < n; ++i) {
+            arr[i] += arr[i - 1];
+        }
+
+        return Arrays.copyOfRange(arr,0,n);
+    }
+}
