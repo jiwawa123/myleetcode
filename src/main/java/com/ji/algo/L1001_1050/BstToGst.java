c8454073cddd2aa5df23f0882fd48ae3a3416850
ji <252099562@qq.com>
2019-07-25 10:17:17 +0800

    ques1038. 从二叉搜索树到更大和树

---
+++ src/main/java/com/ji/algo/L1001_1050/BstToGst.java
@@ -0,0 +1,43 @@
+package com.ji.algo.L1001_1050;/*
+    user ji
+    data 2019/7/25
+    time 10:15 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BstToGst {
+    List<Integer> list = new ArrayList();
+    Map<Integer,Integer> map = new HashMap();
+    public TreeNode bstToGst(TreeNode root) {
+        if(root==null)
+            return root;
+        help(root);
+        int sum = 0;
+        for(int i = list.size()-1;i>=0;i--){
+            sum += list.get(i);
+            map.put(list.get(i),sum);
+        }
+        helpI(root);
+        return root;
+    }
+    public void help(TreeNode root){
+        if(root==null)
+            return;
+        help(root.left);
+        list.add(root.value);
+        help(root.right);
+    }
+    public void helpI(TreeNode root){
+        if(root==null)
+            return;
+        root.value = map.get(root.value);
+        helpI(root.left);
+        helpI(root.right);
+    }
+}
