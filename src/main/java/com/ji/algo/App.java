3fbc04fd843fe2528909ddbb7f36829d36bb873b
ji <252099562@qq.com>
2019-07-27 17:51:00 +0800

    更新sql脚本，统计连续出现三次及以上的那些数字

--- src/main/java/com/ji/algo/App.java
+++ src/main/java/com/ji/algo/App.java
@@ -8,33 +8,32 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
 
-        int arr[] = {-1,2,1,-4};
-        System.out.println(threeSumClosest(arr,1));
+        int arr[] = {-1, 2, 1, -4};
+        System.out.println(threeSumClosest(arr, 1));
     }
+
     public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int min = Integer.MAX_VALUE;
         int max = 0;
-        for(int i= 0;i<nums.length;i++){
-            int start = i+1;
-            int end = nums.length-1;
-            while(start<end){
-                if(nums[start]+nums[end]==target-nums[i])
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                if (nums[start] + nums[end] == target - nums[i])
                     return target;
-                if(nums[start]+nums[end]<target-nums[i]){
+                if (nums[start] + nums[end] < target - nums[i]) {
 
-                    if(min<Math.abs(target - nums[i] - nums[start] -nums[end])){
-                        min = Math.abs(target - nums[i] - nums[start] -nums[end]);
-                        max =  nums[i] + nums[start] + nums[end];
+                    if (min < Math.abs(target - nums[i] - nums[start] - nums[end])) {
+                        min = Math.abs(target - nums[i] - nums[start] - nums[end]);
+                        max = nums[i] + nums[start] + nums[end];
                     }
                     start++;
-                }
+                } else {
 
-                else{
-
-                    if(min<Math.abs(target - nums[i] - nums[start] -nums[end])){
-                        min = Math.abs(target - nums[i] - nums[start] -nums[end]);
-                        max =  nums[i] + nums[start] + nums[end];
+                    if (min < Math.abs(target - nums[i] - nums[start] - nums[end])) {
+                        min = Math.abs(target - nums[i] - nums[start] - nums[end]);
+                        max = nums[i] + nums[start] + nums[end];
                     }
                     end--;
                 }
@@ -42,5 +41,37 @@ public class App {
             }
         }
         return max;
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        for (String str : tokens) {
+            switch (str) {
+                case "+":
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(a + b);
+                    break;
+                case "-":
+                    int a1 = stack.pop();
+                    int b1 = stack.pop();
+                    stack.push(b1 - a1);
+                    break;
+                case "*":
+                    int a2 = stack.pop();
+                    int b2 = stack.pop();
+                    stack.push(b2 * a2);
+                    break;
+                case "/":
+                    int a3 = stack.pop();
+                    int b3 = stack.pop();
+                    stack.push(b3 / a3);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(str));
+                    break;
+            }
+        }
+        return stack.pop();
     }
 }
--- src/main/java/com/ji/algo/mysql/ques.sql
+++ src/main/java/com/ji/algo/mysql/ques.sql
@@ -1,2 +1,8 @@
 # 三目运算 在进行if判断的时候使用一个等于号即可
 update user set sex = if(sex='m','f','m');
+
+# 统计连续出现三次的数字
+select distinct a.Num ConsecutiveNums
+from Logs a,Logs b,Logs c
+where a.Num=b.Num and b.Num=c.Num
+and a.Id+1=b.Id and b.Id+1=c.Id
