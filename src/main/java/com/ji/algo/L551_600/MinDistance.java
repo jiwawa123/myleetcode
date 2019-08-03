84893c6df87629ba9549cd7b2536e4b0449ea880
ji <252099562@qq.com>
2019-08-03 10:28:57 +0800

    ques583. 两个字符串的删除操作,找最长公共子序列即可

---
+++ src/main/java/com/ji/algo/L551_600/MinDistance.java
@@ -0,0 +1,27 @@
+package com.ji.algo.L551_600;/*
+    user ji
+    data 2019/8/3
+    time 10:21 AM
+*/
+
+public class MinDistance {
+    public static void main(String[] args) {
+
+    }
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return m + n - 2 * dp[m][n];
+    }
+}
