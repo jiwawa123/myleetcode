6723d68a08c7c2063de913585ca4af3a64827e14
ji <252099562@qq.com>
2019-08-05 17:55:53 +0800

    ques213II,打家劫舍II，在原来的基础上面考虑只取左边或者只取右边， 同时将打家劫舍I的接口抽出来形成一个新的接口文件。

--- src/main/java/com/ji/algo/L201_250/RobII.java
+++ src/main/java/com/ji/algo/L201_250/RobII.java
@@ -4,10 +4,12 @@ package com.ji.algo.L201_250;/*
     time 8:35 PM
 */
 
+import java.util.Arrays;
+
 public class RobII {
     public static void main(String[] args) {
-        int[] arr = {2, 7, 9, 3, 1};
-        System.out.println(rob(arr));
+        int[] arr = {1, 2, 3, 1};
+        System.out.println(robII(arr));
     }
 
     public static int rob(int[] nums) {
@@ -27,7 +29,7 @@ public class RobII {
 
         }
         f = nums[nums.length - 1];
-        s = Math.max(nums[nums.length-1], nums[nums.length-2]);
+        s = Math.max(nums[nums.length - 1], nums[nums.length - 2]);
         int min = Math.min(f, s);
         for (int i = nums.length - 3; i > 0; i--) {
             min = Math.max(s, nums[i] + f);
@@ -36,5 +38,35 @@ public class RobII {
 
         }
         return Math.max(max, min);
+    }
+
+
+    public static int robII(int[] nums) {
+        int p[] = new int[nums.length - 1];
+        int q[] = new int[nums.length - 1];
+        for (int i = 0; i < nums.length - 1; i++) {
+            p[i] = nums[i];
+            q[i] = nums[i + 1];
+        }
+        int l = robIII(p);
+        int r = robIII(q);
+        return Math.max(l, r);
+    }
+
+    public static int robIII(int[] nums) {
+        if (nums == null || nums.length < 1)
+            return 0;
+        if (nums.length < 2)
+            return nums[0];
+
+        int arr[] = new int[nums.length];
+        arr[0] = nums[0];
+        arr[1] = Math.max(arr[0], nums[1]);
+        for (int i = 2; i < nums.length ; i++) {
+            arr[i] = Math.max(arr[i - 1], nums[i] + arr[i - 2]);
+        }
+        return arr[nums.length - 1];
+
+
     }
 }
