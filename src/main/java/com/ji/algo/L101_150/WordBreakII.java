209a11b6b489c213ee282812373270295ff76ec3
ji <252099562@qq.com>
2019-08-08 15:50:57 +0800

    ques140.单词拆分，在原来判断单词是否可以拆分的基础上上面在进行组合。

--- src/main/java/com/ji/algo/L101_150/WordBreakII.java
+++ src/main/java/com/ji/algo/L101_150/WordBreakII.java
@@ -4,16 +4,13 @@ package com.ji.algo.L101_150;/*
     time 1:30 PM
 */
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WordBreakII {
     public static void main(String[] args) {
         WordBreakII wordBreakII = new WordBreakII();
-        List<String> list = Arrays.asList("leet", "code");
-        System.out.println(wordBreakII.wordBreak("leetcode", list));
+        List<String> list = Arrays.asList("cat", "cats", "and", "sand", "dog");
+        System.out.println(wordBreakII.wordBreakII("catsanddog", list));
     }
 
 
@@ -36,5 +33,35 @@ public class WordBreakII {
             }
         }
         return flag[n];
+    }
+
+    public List<String> wordBreakII(String s, List<String> wordDict) {
+        if(!wordBreak(s,wordDict))
+            return new ArrayList<>();
+        Map<Integer, Set<String>> map = new HashMap<>();
+        Map<String, Integer> word = new HashMap<>();
+        int n = s.length();
+        for (String tmp : wordDict
+                ) {
+            word.put(tmp, 1);
+        }
+        Set<String> zero = new HashSet<>();
+        zero.add("");
+        map.put(0, zero);
+        for (int i = 1; i <= n; i++) {
+            map.put(i, new HashSet<>());
+            for (int j = 0; j < i; j++) {
+                if (map.get(j).size() > 0 && word.containsKey(s.substring(j, i))) {
+                    Set<String> tmp = map.get(j);
+                    Set<String> kk = map.get(i);
+                    for (String last : tmp
+                            ) {
+                        kk.add((last + " " + s.substring(j, i)).trim());
+                    }
+                    map.put(i, kk);
+                }
+            }
+        }
+        return new ArrayList<>(map.get(n));
     }
 }
