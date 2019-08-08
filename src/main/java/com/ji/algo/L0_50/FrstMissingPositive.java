8239e32993e3ca217909b94180f3ddc6b884adb1
ji <252099562@qq.com>
2019-08-08 14:49:57 +0800

    ques41.第一个缺失的数据，头脑不清楚的时候不写代码

--- src/main/java/com/ji/algo/L0_50/FrstMissingPositive.java
+++ src/main/java/com/ji/algo/L0_50/FrstMissingPositive.java
@@ -4,12 +4,14 @@ package com.ji.algo.L0_50;/*
     time 8:45 AM
 */
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FrstMissingPositive {
     public static void main(String[] args) {
-
+        int arr[] = {3, 4, 0, 2};
+        System.out.println(firstMissingPositiveII(arr));
     }
 
     public int firstMissingPositive(int[] nums) {
@@ -23,5 +25,35 @@ public class FrstMissingPositive {
             res++;
         }
         return res;
+    }
+
+    public static int firstMissingPositiveII(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 1;
+        Arrays.sort(nums);
+        if (nums[0] > 1)
+            return 1;
+        int res = 1;
+        int next = 1;
+        boolean flag = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < res)
+                continue;
+            if (nums[i] == res) {
+                flag = true;
+                next = res + 1;
+                continue;
+            }
+            if (flag) {
+                if (nums[i] - res == 1) {
+                    res = nums[i];
+                    next = res + 1;
+                    continue;
+                } else {
+                    return next;
+                }
+            }
+        }
+        return next;
     }
 }
