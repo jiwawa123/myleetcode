e52571ae3f0dd68d619d235e211c9c40ef71e2f1
ji <252099562@qq.com>
2019-08-09 13:54:20 +0800

    ques862. 和至少为 K 的最短子数组

--- src/main/java/com/ji/algo/L851_900/ShortestSubarray.java
+++ src/main/java/com/ji/algo/L851_900/ShortestSubarray.java
@@ -6,15 +6,47 @@ package com.ji.algo.L851_900;/*
 
 public class ShortestSubarray {
     public static void main(String[] args) {
-
+        int arr[] = {2, -1, 2, 1, 3};
+        System.out.println(new ShortestSubarray().shortestSubarray(arr, 3));
     }
 
     public int shortestSubarray(int[] A, int K) {
-        int min = Integer.MAX_VALUE;
-        int len = 0;
-        for (int i = 0; i < A.length; i++) {
-
+        int len = A.length;
+        int sum = 0, begin = 0, res = -1;
+        for (int i = 0; i < len; i++) {
+            if (A[i] >= K) return 1;
+            sum += A[i];
+            if (sum < 1) {
+                sum = 0;
+                begin = i + 1;
+                continue;
+            }
+            for (int j = i - 1; A[j + 1] < 0; j--) {
+                A[j] = A[j + 1] + A[j];
+                A[j + 1] = 0;
+            }
+            if (sum >= K) {
+                while (sum - A[begin] >= K || A[begin] <= 0)
+                    sum -= A[begin++];
+                int length = i - begin + 1;
+                if (res < 0 || res > length)
+                    res = length;
+            }
         }
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return res;
+    }
+
+    public boolean isExist(int[] A, int K) {
+        int count = Integer.MIN_VALUE;
+        int max = count;
+        for (int i = 0; i < A.length; i++) {
+            if (count < 0) {
+                count = A[i];
+            } else {
+                count += A[i];
+            }
+            max = Math.max(count, max);
+        }
+        return max >= K;
     }
 }
