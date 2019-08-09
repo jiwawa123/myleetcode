401e9f729b7a6bd24d717492c553f68873157793
ji <252099562@qq.com>
2019-08-09 14:30:04 +0800

    使用map来维护每个中间的结果，避免重复计算

--- src/main/java/com/ji/algo/L851_900/AllPossibleFBT.java
+++ src/main/java/com/ji/algo/L851_900/AllPossibleFBT.java
@@ -7,11 +7,17 @@ package com.ji.algo.L851_900;/*
 import com.ji.algo.Util.TreeNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AllPossibleFBT {
 
+    Map<Integer, List<TreeNode>> map = new HashMap<>();
+
     public List<TreeNode> allPossibleFBT(int N) {
+        if(map.containsKey(N))
+            return map.get(N);
         List<TreeNode> root = new ArrayList<>();
         if (N == 0)
             return root;
@@ -20,9 +26,8 @@ public class AllPossibleFBT {
             return root;
         }
         for (int i = 1; i < N - 1; i++) {
-
-            List<TreeNode> left = allPossibleFBT(i);
-            List<TreeNode> right = allPossibleFBT(N - i - 1);
+            List<TreeNode> left = map.getOrDefault(i,allPossibleFBT(i));
+            List<TreeNode> right = map.getOrDefault(N - i - 1,allPossibleFBT(i));
             for (int j = 0; j < left.size(); j++) {
                 for (int k = 0; k < right.size(); k++) {
                     TreeNode ll = new TreeNode(0);
@@ -32,6 +37,7 @@ public class AllPossibleFBT {
                 }
             }
         }
+        map.put(N, root);
         return root;
     }
 }
