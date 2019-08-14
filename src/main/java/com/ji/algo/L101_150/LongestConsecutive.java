ac8745e656d90dea2b3a78bd393583c478191579
ji <252099562@qq.com>
2019-08-14 16:55:29 +0800

    ques128.最长连续序列，直接排序的方法

--- src/main/java/com/ji/algo/L101_150/LongestConsecutive.java
+++ src/main/java/com/ji/algo/L101_150/LongestConsecutive.java
@@ -4,6 +4,7 @@ package com.ji.algo.L101_150;/*
     time 3:12 PM
 */
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ public class LongestConsecutive {
     public static void main(String[] args) {
 
     }
+
     // 使用map记录左右两边的数量即可
     public int longestConsecutive(int[] nums) {
         Map<Integer, Integer> map = new HashMap();
@@ -29,18 +31,37 @@ public class LongestConsecutive {
                     map.put(right + k, tmp);
                     map.put(k - left, tmp);
                     map.put(k, 1);
-                }else if(left!=null){
+                } else if (left != null) {
                     int tmp = left + 1;
                     max = Math.max(tmp, max);
                     map.put(k - left, tmp);
                     map.put(k, 1);
-                }else{
-                    int temp = right+1;
-                    max= Math.max(max,temp);
-                    map.put(right+k,temp);
-                    map.put(k,temp);
+                } else {
+                    int temp = right + 1;
+                    max = Math.max(max, temp);
+                    map.put(right + k, temp);
+                    map.put(k, temp);
                 }
             }
+        }
+        return max;
+    }
+
+    public int longestConsecutiveII(int nums[]) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        Arrays.sort(nums);
+        int count = 1;
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1])
+                continue;
+            if (nums[i] == nums[i - 1] + 1)
+                count++;
+            else
+                count = 1;
+
+            max = Math.max(max, count);
         }
         return max;
     }
