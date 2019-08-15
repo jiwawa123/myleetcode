35a77826ba0d06a9daabc804f61c3a024d587e4e
ji <252099562@qq.com>
2019-08-15 09:36:37 +0800

    ques825. 适龄的朋友,遇到大量出现的数组，可以考虑使用map作为中间记录的保存

--- src/main/java/com/ji/algo/L801_850/NumFriendRequests.java
+++ src/main/java/com/ji/algo/L801_850/NumFriendRequests.java
@@ -4,32 +4,42 @@ package com.ji.algo.L801_850;/*
     time 5:39 PM
 */
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NumFriendRequests {
     public static void main(String[] args) {
-        int arr[] = {16, 16};
+        int arr[] = new int[2];
         System.out.println(numFriendRequests(arr));
     }
 
     public static int numFriendRequests(int[] ages) {
         Arrays.sort(ages);
         int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < ages.length; i++) {
+            if (map.containsKey(ages[i])) {
+                count += map.get(ages[i]);
+                continue;
+            }
+            int tmp = 0;
             for (int j = i - 1; j >= 0; j--) {
-                if (ages[i] > 100 && ages[j] < 100)
-                    break;
-                if (ages[i] > ages[j] * 0.5 + 7) {
-                    count++;
+                if (ages[j] > ages[i] * 0.5 + 7) {
+                    tmp++;
                 } else {
                     break;
                 }
             }
             for (int j = i + 1; j < ages.length; j++) {
-                if (ages[i] == ages[j])
-                    count++;
+                if (ages[j] == ages[i] && ages[i] > 0.5 * ages[i] + 7)
+                    tmp++;
+                else
+                    break;
             }
+            map.put(ages[i], tmp);
+            count += tmp;
         }
         return count;
     }
