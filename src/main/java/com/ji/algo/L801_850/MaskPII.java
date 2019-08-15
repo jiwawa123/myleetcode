6971f359cd04b045d62ea191cb6915f1ee3256b5
ji <252099562@qq.com>
2019-08-15 09:54:04 +0800

    ques831. 隐藏个人信息,严格按照要求来做即可

---
+++ src/main/java/com/ji/algo/L801_850/MaskPII.java
@@ -0,0 +1,39 @@
+package com.ji.algo.L801_850;/*
+    user ji
+    data 2019/8/15
+    time 9:39 AM
+*/
+
+public class MaskPII {
+    public static void main(String[] args) {
+        System.out.println(maskPII("+86(88)1513-7-74"));
+    }
+
+    public static String maskPII(String S) {
+        if (S.contains("@")) {
+            S = S.toLowerCase();
+            String tmp[] = S.split("@");
+            return tmp[0].charAt(0) + "*****" + tmp[0].charAt(tmp[0].length() - 1)
+                    + "@" + tmp[1];
+        } else {
+            S = S.replaceAll("\\+", "");
+            S = S.replaceAll("\\-", "");
+            S = S.replaceAll("\\(", "");
+            S = S.replaceAll("\\)", "");
+            int len = S.length();
+            String last = S.substring(S.length() - 4, S.length());
+            switch (len) {
+                case 10:
+                    return "***-***-" + last;
+                case 11:
+                    return "+*-***-***-" + last;
+                case 12:
+                    return "+**-***-***-" + last;
+                case 13:
+                    return "+***-***-***-" + last;
+
+            }
+            return "";
+        }
+    }
+}
