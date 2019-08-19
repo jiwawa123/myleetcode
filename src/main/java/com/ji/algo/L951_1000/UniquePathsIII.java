8bf020fd7780f409916088f2e133fe67b18c4b65
ji <252099562@qq.com>
2019-08-19 19:53:31 +0800

    ques980.不同路径III

---
+++ src/main/java/com/ji/algo/L951_1000/UniquePathsIII.java
@@ -0,0 +1,71 @@
+package com.ji.algo.L951_1000;/*
+    user ji
+    data 2019/8/19
+    time 5:58 PM
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UniquePathsIII {
+    public static void main(String[] args) {
+        //int arr[][] = {{1, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 2, -1}};
+        int arr[][] = {{1, -1}, {0, 2}};
+        UniquePathsIII iii = new UniquePathsIII();
+        System.out.println(iii.uniquePathsIII(arr));
+    }
+
+    int ans;
+    int[][] grid;
+    int tr, tc;
+    int[] dr = new int[]{0, -1, 0, 1};
+    int[] dc = new int[]{1, 0, -1, 0};
+    int R, C;
+
+    public int uniquePathsIII(int[][] grid) {
+        this.grid = grid;
+        R = grid.length;
+        C = grid[0].length;
+
+        int todo = 0;
+        int sr = 0, sc = 0;
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c) {
+                if (grid[r][c] != -1) {
+                    todo++;
+                }
+
+                if (grid[r][c] == 1) {
+                    sr = r;
+                    sc = c;
+                } else if (grid[r][c] == 2) {
+                    tr = r;
+                    tc = c;
+                }
+            }
+
+        ans = 0;
+        dfs(sr, sc, todo);
+        return ans;
+    }
+
+    public void dfs(int r, int c, int todo) {
+        todo--;
+        if (todo < 0) return;
+        if (r == tr && c == tc) {
+            if (todo == 0) ans++;
+            return;
+        }
+
+        grid[r][c] = 3;
+        for (int k = 0; k < 4; ++k) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+            if (0 <= nr && nr < R && 0 <= nc && nc < C) {
+                if (grid[nr][nc] % 2 == 0)
+                    dfs(nr, nc, todo);
+            }
+        }
+        grid[r][c] = 0;
+    }
+}
