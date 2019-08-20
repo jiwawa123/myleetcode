943049c0f5c53f10f9f284739471bf810fd8c961
ji <252099562@qq.com>
2019-08-20 10:16:33 +0800

    ques392.判断是不是子序列。 新加方法可以通过动态规划来判断即可

--- src/main/java/com/ji/algo/L351_400/IsSubSequence.java
+++ src/main/java/com/ji/algo/L351_400/IsSubSequence.java
@@ -25,4 +25,24 @@ public class IsSubSequence {
         }
         return true;
     }
+    public int longestSubString(String s1, String s2) {
+        int arr[][] = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i < s1.length() + 1; i++) {
+            arr[i][0] = 0;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            arr[0][i] = 0;
+        }
+        int count = 0;
+        for (int i = 1; i < s1.length() + 1; i++) {
+            for (int j = 1; j < s2.length() + 1; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    arr[i][j] = 1 + arr[i - 1][j - 1];
+                } else {
+                    arr[i][j] = Math.max(arr[i][j - 1], arr[i - 1][j]);
+                }
+            }
+        }
+        return arr[s1.length()][s2.length()];
+    }
 }
