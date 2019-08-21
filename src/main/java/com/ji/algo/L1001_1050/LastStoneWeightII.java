3aba104d5708816d9f7a80ceab04daa7c95190d9
ji <252099562@qq.com>
2019-08-21 21:33:42 +0800

    ques1049. 最后一块石头的重量 II

--- src/main/java/com/ji/algo/L1001_1050/LastStoneWeightII.java
+++ src/main/java/com/ji/algo/L1001_1050/LastStoneWeightII.java
@@ -4,43 +4,36 @@ package com.ji.algo.L1001_1050;/*
     time 8:48 AM
 */
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LastStoneWeightII {
     public static void main(String[] args) {
-        int arr[] = {2,7,4,1,8,1};
-        lastStoneWeightII(arr);
+        int arr[] = {2, 7, 4, 1, 8, 1};
+        System.out.println(lastStoneWeightII(arr));
     }
 
     public static int lastStoneWeightII(int[] stones) {
-        List<Integer> list = new LinkedList<Integer>();
-        for (int i = 0; i < stones.length; i++) {
-            list.add(stones[i]);
+        /* 由于石头拿走还能放回去，因此可以简单地把所有石头看作两堆
+         * 假设总重量为 sum, 则问题转化为背包问题：如何使两堆石头总重量接近 sum / 2
+         */
+        int len = stones.length;
+        /* 获取石头总重量 */
+        int sum = 0;
+        for (int i : stones) {
+            sum += i;
         }
-        return help(list);
-    }
-
-    public static int help(List<Integer> list) {
-        if (list.size() == 1)
-            return list.get(0);
-        if (list.size() == 0)
-            return 0;
-        Collections.sort(list);
-        int min = Integer.MAX_VALUE;
-        int arr[] = new int[2];
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) - list.get(i - 1) < min) {
-                min = list.get(i) - list.get(i - 1);
-                arr[0] = i - 1;
-                arr[1] = i;
+        /* 定义 dp[i] 重量上限为 i 时背包所能装载的最大石头重量 */
+        int maxCapacity = sum/2;
+        int[] dp = new int[maxCapacity + 1];
+        for (int i = 0; i < len; i++) {
+            int curStone = stones[i];
+            for (int j = maxCapacity; j >= curStone; j--) {
+                dp[j] = Math.max(dp[j], dp[j-curStone] + curStone);
             }
         }
-        list.remove(arr[1]);
-        list.remove(arr[0]);
-        if (min != 0)
-            list.add(min);
-        return help(list);
+        return sum - 2 * dp[maxCapacity];
     }
 }
