69e62f8fe9a3e094bae78ab1e34683a17c327dde
ji <252099562@qq.com>
2019-08-21 14:14:52 +0800

    ques789. 逃脱阻碍者 直接计算直线距离即可

---
+++ src/main/java/com/ji/algo/L751_800/EscapeGhosts.java
@@ -0,0 +1,21 @@
+package com.ji.algo.L751_800;/*
+    user ji
+    data 2019/8/21
+    time 2:10 PM
+*/
+
+public class EscapeGhosts {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int distance = Math.abs(target[0]) + Math.abs(target[1]);
+        for (int i = 0; i < ghosts.length; i++) {
+            int dis = Math.abs(ghosts[i][0] - target[0]) + Math.abs(ghosts[i][1] - target[1]);
+            if (dis >= distance)
+                return false;
+        }
+        return true;
+    }
+}
