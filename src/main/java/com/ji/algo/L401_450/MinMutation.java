ea285670637d731873d9f0e85cb3ae01d25ce9d8
ji <252099562@qq.com>
2019-08-24 08:48:45 +0800

    ques432.最小变化

---
+++ src/main/java/com/ji/algo/L401_450/MinMutation.java
@@ -0,0 +1,40 @@
+package com.ji.algo.L401_450;/*
+    user ji
+    data 2019/8/24
+    time 8:37 AM
+*/
+
+public class MinMutation {
+    public static void main(String[] args) {
+
+    }
+
+    public int minMutation(String start, String end, String[] bank) {
+        int[] ans = {Integer.MAX_VALUE};
+        helper(start, end, bank, new boolean[bank.length], ans, 0);
+        return ans[0] == Integer.MAX_VALUE ? -1 : ans[0];
+    }
+
+    private void helper(String now, String end, String[] bank, boolean[] flag, int[] ans, int res) {
+        if (now.equals(end)) {
+            ans[0] = Math.min(ans[0], res);
+        } else {
+            for (int i = 0; i < bank.length; i++) {
+                if (!flag[i] && mutation(now, bank[i])) {
+                    flag[i] = true;
+                    helper(bank[i], end, bank, flag, ans, res + 1);
+                    flag[i] = false;
+                }
+            }
+        }
+    }
+
+
+    private boolean mutation(String a, String b) {
+        int ans = 0;
+        for (int i = 0; i < 8; i++)
+            if (a.charAt(i) != b.charAt(i))
+                ans++;
+        return ans == 1;
+    }
+}
