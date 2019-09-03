e69faae6a16b5670ea27761554e85e5215e0e147
ji <252099562@qq.com>
2019-09-03 18:19:15 +0800

    ques298.Binary Tree Longest Consecutive Sequence

---
+++ src/main/java/com/ji/algo/L251_300/BinaryTreeLongestConsecutiveSequence.java
@@ -0,0 +1,37 @@
+package com.ji.algo.L251_300;/*
+    user ji
+    data 2019/9/3
+    time 6:15 PM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class BinaryTreeLongestConsecutiveSequence {
+    public static void main(String[] args) {
+
+    }
+
+    int count = 0;
+    public int getLongestConsecutiveSequence(TreeNode root) {
+        if (root == null)
+            return 0;
+        help(root, 1);
+        return count;
+    }
+
+    public void help(TreeNode root, int dep) {
+        if (root == null)
+            return;
+        count = Math.max(count, dep);
+        if (root.left != null && root.left.value == root.value + 1) {
+            help(root.left, dep + 1);
+        } else {
+            help(root.left, 1);
+        }
+        if (root.right != null && root.right.value == root.value + 1) {
+            help(root.right, dep + 1);
+        } else {
+            help(root.right, 1);
+        }
+    }
+}
