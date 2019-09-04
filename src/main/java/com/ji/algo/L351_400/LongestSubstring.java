33bddd1bf2679f6a91f1f70703f163a9f89b780c
ji <252099562@qq.com>
2019-09-04 12:59:26 +0800

    ques395. 至少有K个重复字符的最长子串，使用分治的思想即可

--- src/main/java/com/ji/algo/L351_400/LongestSubstring.java
+++ src/main/java/com/ji/algo/L351_400/LongestSubstring.java
@@ -12,13 +12,29 @@ public class LongestSubstring {
 
     }
 
+    //使用分治的思想，分成左右两边即可
     public int longestSubstring(String s, int k) {
         int max = 0;
         if (s == null || s.length() < k)
             return 0;
-        for (int i = 0; i < s.length(); i++) {
+        if (k < 2)
+            return s.length();
+        return longestSubStringSub(s, k, 0, s.length() - 1);
+    }
 
+    public int longestSubStringSub(String s, int k, int start, int end) {
+        if (start > end)
+            return 0;
+        int arr[] = new int[26];
+        for (int i = start; i < end; i++) {
+            arr[s.charAt(i) - 'a'] += 1;
         }
-        return max;
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > 0 && arr[i] < k) {//开始进行分治
+                int pos = s.indexOf((char) ('a' + i), start);
+                return Math.max(longestSubStringSub(s, k, start, pos - 1), longestSubStringSub(s, k, pos + 1, end));
+            }
+        }
+        return end - start + 1;
     }
 }
