5af93e46801529dd661b0808d01d3db2ecc6fd49
ji <252099562@qq.com>
2019-09-25 09:08:43 +0800

    修改代码，通过表示符号来表明是左边还是右边进行相应的插入操作

--- src/main/java/com/ji/algo/game/L34.java
+++ src/main/java/com/ji/algo/game/L34.java
@@ -19,8 +19,8 @@ public class L34 {
             if(nums[med]==target){
                 res[0] = Math.min(res[0],med);
                 res[1] = Math.max(res[1],med);
-                helpL(nums,target,0,med-1);
-                helpR(nums,target,med+1,nums.length-1);
+                help(nums,target,0,med-1,0);
+                help(nums,target,med+1,nums.length-1,1);
                 break;
             }else if(nums[med]<target){
                 i =  med+1;
@@ -32,15 +32,20 @@ public class L34 {
         if(res[1]==Integer.MIN_VALUE) res[1] = -1;
         return res;
     }
-    public void helpL(int nums[],int target,int start,int end){
+    public void help(int nums[],int target,int start,int end,int LrR){
         if(start>end)
             return;
         int i = start, j = end;
         while(i<=j){
             int med = (j-i)/2+i;
             if(nums[med]==target){
-                res[0] = Math.min(res[0],med);
-                helpL(nums,target,0,med-1);
+                if(LrR==0){
+                    res[0] = Math.min(res[0],med);
+                    help(nums,target,0,med-1,0);
+                }else{
+                    res[1] = Math.max(res[1],med);
+                    help(nums,target,med+1,nums.length-1,1);
+                }
                 break;
             }else if(nums[med]<target){
                 i =  med+1;
@@ -49,21 +54,5 @@ public class L34 {
             }
         }
     }
-    public void helpR(int nums[],int target,int start,int end){
-        if(start>end)
-            return;
-        int i = start, j = end;
-        while(i<=j){
-            int med = (j-i)/2+i;
-            if(nums[med]==target){
-                res[1] = Math.max(res[1],med);
-                helpR(nums,target,med+1,nums.length-1);
-                break;
-            }else if(nums[med]<target){
-                i =  med+1;
-            }else{
-                j = med -1;
-            }
-        }
-    }
+
 }
