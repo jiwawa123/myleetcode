77205d635e005c440084eda4673e136fd8048d24
ji <252099562@qq.com>
2019-10-14 10:54:03 +0800

    ques10.字符串匹配的api

---
+++ src/main/java/com/ji/algo/L0_50/IsMatch.java
@@ -0,0 +1,11 @@
+package com.ji.algo.L0_50;/*
+    user ji
+    data 2019/10/13
+    time 9:12 PM
+*/
+
+public class IsMatch {
+    public static void main(String[] args) {
+        System.out.println("tt".matches("t*"));
+    }
+}
