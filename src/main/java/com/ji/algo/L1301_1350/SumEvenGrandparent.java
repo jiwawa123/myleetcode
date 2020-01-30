86a048e351d6cb0599c7bf6d7fa69f213c76e1bb
shixi_zhenhua5 <shixi_zhenhua5@staff.weibo.com>
2020-01-30 11:53:34 +0800

    ques1315. 祖父节点值为偶数的节点和,同时传递两个参数即可

---
+++ src/main/java/com/ji/algo/L1301_1350/SumEvenGrandparent.java
@@ -0,0 +1,29 @@
+package com.ji.algo.L1301_1350;/*
+    user ji
+    data 2020/1/30
+    time 11:43 AM
+*/
+
+import com.ji.algo.Util.TreeNode;
+
+public class SumEvenGrandparent {
+    public static void main(String[] args) {
+
+    }
+
+    int sum = 0;
+
+    public int sumEvenGrandparent(TreeNode root) {
+        help(root, -1, -1);
+        return sum;
+    }
+
+    public void help(TreeNode root, int grand, int father) {
+        if (root == null)
+            return;
+        if (grand % 2 == 0)
+            sum += root.value;
+        help(root.left, father, root.value);
+        help(root.right, father, root.value);
+    }
+}
